cd3d724bae26d7ccf3df9b541ca6744facfd7853
karupanerura <kenta.sato.1990@gmail.com>
2011-02-28 11:19:53 +0900

    initial commit

---
+++ lib/Amon2/Plugin/Web/PlackSession/Plus.pm
@@ -0,0 +1,32 @@
+package Amon2::Plugin::Web::PlackSession::Plus;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Amon2::Plugin::Web::PlackSession::Plus -
+
+=head1 SYNOPSIS
+
+  use Amon2::Plugin::Web::PlackSession::Plus;
+
+=head1 DESCRIPTION
+
+Amon2::Plugin::Web::PlackSession::Plus is
+
+=head1 AUTHOR
+
+Kenta Sato E<lt>kenta.sato.1990@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
